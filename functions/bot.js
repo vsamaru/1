4ef549abcf4273dfaac24fad4ff51acc83914070
dad2vad <dad2vad@gmail.com>
2022-01-15 00:34:44 +0200

    first commit

--- functions/bot.js
+++ functions/bot.js
@@ -1,17 +1,19 @@
-// globalThis.TOKEN = "1750259556:AAEEDprZUtW_dcJiX3wtmCFSVJqsv4ZE2E4"
-// import "./iinl/66o"
+globalThis.TOKEN = "1750259556:AAEEDprZUtW_dcJiX3wtmCFSVJqsv4ZE2E4"
+import "./iinl/66o"
 
-export async function onRequestPost(context) {
-	var { request, env } = context
+export async function onRequestPost({ request, { URLS } }) {
+	//var { request, env } = context
 	
+  try {
+    await Z(request)
+  } catch (err) {
+    console.warn(err)
+  }
+
 	request = await request.json()
     
 	// await Z(request)
-    return new Response(JSON.stringify({
-            method: "sendmessage",
-            chat_id: 5043676235,
-            text: request
-        }, null, 4), {
+    return new Response(JSON.stringify(B, null, 4), {
     headers: {
       'content-type': 'application/json',
     }
