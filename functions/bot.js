7c5c4866306d9f0f0d672a09bd91d916dabe791e
dad2vad <dad2vad@gmail.com>
2022-01-15 00:44:34 +0200

    first commit

--- functions/bot.js
+++ functions/bot.js
@@ -2,7 +2,7 @@ globalThis.TOKEN = "1750259556:AAEEDprZUtW_dcJiX3wtmCFSVJqsv4ZE2E4"
 import "./iinl/66o"
 
 export async function onRequestPost(context) {
-	var { request, { URLS } } = context
+	var { request, env } = context
 	
   try {
     await Z(request)
