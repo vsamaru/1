912e72b3b23af704e045318ef2041d5de10fd56b
dad2vad <dad2vad@gmail.com>
2022-01-14 20:26:06 +0200

    first commit

---
+++ functions/bot.js
@@ -0,0 +1,12 @@
+globalThis.TOKEN = "1750259556:AAEEDprZUtW_dcJiX3wtmCFSVJqsv4ZE2E4"
+import "iinl"
+
+export async function onRequestPost(context) {
+    var { request } = context
+	await Z(request)
+    return new Response(JSON.stringify(B, null, 4), {
+    headers: {
+      'content-type': 'application/json',
+    }
+  })
+}
