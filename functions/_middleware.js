f72fce8fc287e022196ecc0b9370df056334a0d3
dad2vad <dad2vad@gmail.com>
2022-01-16 20:35:58 +0200

    first commit

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,20 +1,34 @@
 if (typeof L === 'undefined') globalThis.L = {}
 
-const errorHandler = async ({ next }) => {
+async function errorHandler(context) {
+  var { request, env: { URLS } } = context
+  L = URLS
   try {
-    return await next()
+    // wait for the next function to finish
+    return await context.next()
   } catch (err) {
+    // catch and report and errors when running the next function
     return new Response(`${err.message}\n${err.stack}`, { status: 500 })
   }
 }
 
-const hello = async ({ next }) => {
-  const response = await next()
-  response.headers.set('X-Hello', 'Hello from functions Middleware!')
-  return response
-}
+// Attach `errorHandler` to all HTTP requests
+export const onRequest = errorHandler;
+// const errorHandler = async ({ next }) => {
+//   try {
+//     return await next()
+//   } catch (err) {
+//     return new Response(`${err.message}\n${err.stack}`, { status: 500 })
+//   }
+// }
 
-export const onRequest = [
-  errorHandler,
-  hello
-]
+// const hello = async ({ next }) => {
+//   const response = await next()
+//   response.headers.set('X-Hello', 'Hello from functions Middleware!')
+//   return response
+// }
+
+// export const onRequest = [
+//   errorHandler,
+//   hello
+// ]
--- functions/bot.js
+++ functions/bot.js
@@ -2,11 +2,11 @@ globalThis.TOKEN = "1750259556:AAEEDprZUtW_dcJiX3wtmCFSVJqsv4ZE2E4"
 import "./iinl/66o"
 
 export async function onRequestPost(context) {
-	var { request, env: { URLS } } = context
-globalThis.U = URLS
+	var { request } = context
+
   try {
     await Z(request)
-    var j = await U.get("J")
+    var j = await L.get("J")
     console.info(j)
   } catch (err) {
     console.warn(err)
