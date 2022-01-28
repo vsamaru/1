bf3590d6e49174e30131f9e669411dd5b2dbd986
dad2vad <dad2vad@gmail.com>
2022-01-28 09:21:37 +0200

    first commit

--- functions/api.js
+++
@@ -1,8 +0,0 @@
-export async function onRequestGet({ params }) {
-  const res = await fetch(
-    `https://rickandmortyapi.com/api/character/${params.id}`
-  );
-  const data = await res.json();
-  const info = JSON.stringify(data);
-  return new Response(info, null, 2);
-}
