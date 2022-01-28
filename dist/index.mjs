1d3c5bb2a8a70c15894e25eacd77a9f6bea50ff6
dad2vad <dad2vad@gmail.com>
2022-01-28 13:33:48 +0200

    first commit

--- dist/index.mjs
+++ dist/index.mjs
@@ -183,7 +183,7 @@ globalThis.Z = async function(r) {
   	 console.info(nn)
     var t = (v.ref + "%20" +  req.caption).toUpperCase().replace(/ /g, "%20").replace(/,/g, "%20");
     req.width = req.width > 960 ? req.width : 960
-    req.photo = `https://res.cloudinary.com/o6/image/fetch/w_${req.width}/fl_relative,g_north_west,h_400,l_${v.pic},w_400/b_rgb:f5e6e4,c_fill,co_black,fl_relative,g_north_west,l_text:Yanone%20Kaffeesatz_42_center:${t},w_400,y_400/l_n_jp83ut,x_0.16,y_0.16/${req.photo}`;
+    req.photo = `https://res.cloudinary.com/o6/image/fetch/w_${req.width}/fl_relative,g_north_west,h_400,l_${v.pic},w_400/b_rgb:f5e6e4,c_fit,co_black,fl_relative,g_north_west,l_text:Yanone%20Kaffeesatz_42_center:${t},w_400,y_400/l_n_jp83ut,x_0.16,y_0.16/${req.photo}`;
     B.text = await fetch(`https://api.imgbb.com/1/upload?key=61d5447ecc57bd825f97775369be81f5&name=${v.ll}&image=${encodeURIComponent(req.photo)}`).then((r2) => r2.json()).then(async (r2) => {
       var i = r2.data.id;
       r2 = [  "ibb.co/" + i, "www.google.com/maps?q=" + v.ll].join("\n")
