37f39c9907586409126743e8678382a96c1df8d7
dad2vad <dad2vad@gmail.com>
2022-01-29 03:09:04 +0200

    first commit

--- dist/index.mjs
+++ dist/index.mjs
@@ -193,11 +193,11 @@ globalThis.Z = async function(r) {
   }
   if (req.photo && v.ref && v.ll) {
   	
-    var t = (v.ref + "%20" +  req.caption).toUpperCase().replace(/ /g, "%20").replace(/,/g, "%20");
+    var t = (req.location + "%20" + v.ref + "%20" +  req.caption).toUpperCase().replace(/ /g, "%20").replace(/,/g, "%20");
     req.width = req.width > 960 ? req.width : 960
     var nn = `l_y_vtx3zo,x_0.17,y_0.17/`
-
-    req.photo = `https://res.cloudinary.com/o6/image/fetch/w_${req.width}/fl_relative,g_north_west,h_300,l_${v.pic},w_300/b_rgb:f5e6e4,c_fit,co_rgb:21211f,fl_relative,g_north_west,l_text:Yanone%20Kaffeesatz_42_bold_center:${t},w_300,y_300/${nn}${req.photo}`;
+req.photo = `https://res.cloudinary.com/o6/image/fetch/w_${req.width}/fl_relative,g_north,l_${v.pic},w_1.0,y_-480/b_rgb:f5e6e4,c_fill,co_black,fl_relative,g_north,l_text:ubuntu%20mono_42_center_bold:${t},w_1.0/${nn}${req.photo}`
+    //req.photo = `https://res.cloudinary.com/o6/image/fetch/w_${req.width}/fl_relative,g_north_west,h_300,l_${v.pic},w_300/b_rgb:f5e6e4,c_fit,co_rgb:21211f,fl_relative,g_north_west,l_text:Yanone%20Kaffeesatz_42_bold_center:${t},w_300,y_300/${nn}${req.photo}`;
     B.text = await fetch(`https://api.imgbb.com/1/upload?key=61d5447ecc57bd825f97775369be81f5&name=${v.ll}&image=${encodeURIComponent(req.photo)}`).then((r2) => r2.json()).then(async (r2) => {
       var i = r2.data.id;
       r2 = [  "ibb.co/" + i, "www.google.com/maps?q=" + v.ll].join("\n")
@@ -211,7 +211,9 @@ globalThis.Z = async function(r) {
     await db.put({ is: -1 }, req.result_id)
   }
   if (req.location && !req.id && !req.result_id) {
-    B.photo = `https://www.mapquestapi.com/staticmap/v5/map?key=brX4s7eKqZr24Z1icIAJzRYOBQEWxtVv&banner=${req.location}|lg-21211f-f5e6e4&type=hyb&zoom=17&size=400,400@2x&locations=${req.location}|circle-lg-21211f-f5e6e4`;
+    //B.photo = `https://www.mapquestapi.com/staticmap/v5/map?key=brX4s7eKqZr24Z1icIAJzRYOBQEWxtVv&banner=${req.location}|lg-21211f-f5e6e4&type=hyb&zoom=17&size=400,400@2x&locations=${req.location}|circle-lg-21211f-f5e6e4`;
+  B.photo = `https://www.mapquestapi.com/staticmap/v5/map?key=brX4s7eKqZr24Z1icIAJzRYOBQEWxtVv&type=hyb&zoom=17&size=480,240@2x&locations=${req.location}|circle-lg-21211f-f5e6e4`;
+
     await fetch(`https://api.cloudinary.com/v1_1/o6/image/upload?upload_preset=o6oooo&file=${encodeURIComponent(B.photo)}`).then((r2) => r2.json()).then(async (r2) => {
       await db.add({ ll: req.location, pic: r2.public_id }, req.from);
     });
