4534e29da355baf2a3e5cef204c5d8345a531f4d
vsamaru <vsamaru@gmail.com>
2022-02-04 14:17:02 +0200

    Update index.mjs

--- dist/index.mjs
+++ dist/index.mjs
@@ -31,40 +31,29 @@ var require_html = __commonJS({
       <img class="dev_blog_card_image" src="https://telegram.org/file/464001234/e805/Z0pNrCJET-E.104373/a38ce74977bc53b064">
      
          <h4 class="dev_blog_card_title">Відеоналіпки, удосконалені реакції та інші нововведення</h4>
-         <div class="dev_blog_card_lead">У новій версії з'явилися відеоналіпки, удосконалені реакції з компактнішою анімацією та ширшим вибором емодзі, кнопка для перевірки нових…</div>
-     
-      <div class="dev_blog_card_date">31 січня 2022</div>
+ 
   
 </a>
 <a class="dev_blog_card_link_wrap" href="https://telegram.org/blog/reactions-spoilers-translations/uk">
    
-      <img class="dev_blog_card_image" src="https://telegram.org/file/464001488/d35b/oNi_rR0In0o.124097/c74f7d759893b78bfb">
-      
-         <h4 class="dev_blog_card_title">Реакції, спойлери, переклад повідомлень та QR-коди</h4>
-         <div class="dev_blog_card_lead">Дванадцяте оновлення Telegram за цей рік приносить у месенджер реакції, переклад повідомлень, персоналізовані QR-коди, прихований текст…</div>
-    
-      <div class="dev_blog_card_date">30 грудня 2021</div>
-   
+      <img class="dev_blog_card_image" src="https://telegram.org/file/464001488/d35b/oNi_rR0In0o.124097/c74f7d759893b78bfb"<h4 class="dev_blog_card_title">Захист повідомлень, видалення за датою, керування пристроями та інші нововведення</h4>
+         <h4 class="dev_blog_card_title">Захист повідомлень, видалення за датою, керування пристроями та інші нововведення</h4>
+         
 </a>
 <a class="dev_blog_card_link_wrap" href="https://telegram.org/blog/protected-content-delete-by-date-and-more/uk">
    
       <img class="dev_blog_card_image" src="https://telegram.org/file/464001572/2/u_lvhH-CjJ0.99595/a7fca60f9c9e6e193c">
      
-         <h4 class="dev_blog_card_title">Захист повідомлень, видалення за датою, керування пристроями та інші нововведення</h4>
-         <div class="dev_blog_card_lead">У новій версії з'явився спосіб захистити вміст у групах та каналах від копіювання, можливість…</div>
-      
-      <div class="dev_blog_card_date">7 грудня 2021</div>
+         <h4 class="dev_blog_card_title">З22ахист повідомлень, видалення за датою, керування пристроями та інші нововведення</h4>
+        
    
 </a>
 <a class="dev_blog_card_link_wrap" href="https://telegram.org/blog/shared-media-scrolling-calendar-join-requests-and-more/uk">
-   <div class="dev_blog_card_wrap">
+  
       <img class="dev_blog_card_image" src="https://telegram.org/file/464001930/3/yPTwxfusWZg.174275/3f847742c263cdae99">
-      <div class="dev_blog_card_alltext_wrap">
+      
          <h4 class="dev_blog_card_title">Календар та швидке прокручування в поширених медіа, заявки на вступ, глобальні колірні теми та інші нововведення</h4>
-         <div class="dev_blog_card_lead">У новій версії Telegram додано швидку навігацію матеріалами з історії листування за допомогою…</div>
-      </div>
-      <div class="dev_blog_card_date">3 листопада 2021</div>
-   </div>
+   
 </a>
 </related>
     </article>
