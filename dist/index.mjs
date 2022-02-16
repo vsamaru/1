8eda25d8d6dd93496fb2070abfe320101596575d
vsamaru <vsamaru@gmail.com>
2022-02-16 02:24:10 +0200

    Update index.mjs

--- dist/index.mjs
+++ dist/index.mjs
@@ -2,6 +2,66 @@ import "./mid.js"
 // src/iinl/html.js
 var require_html = __commonJS({
   "src/iinl/html.js"(exports) {
+exports.html2 = (x) => `
+<!DOCTYPE HTML>
+<html xml:lang="uk" lang="uk" dir="ltr" class="device-nonmobile no-js" prefix="og: http://ogp.me/ns#">
+   <head>
+      <meta charset="utf-8">
+      <meta name="apple-mobile-web-app-status-bar-style" content="black">
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="google" content="notranslate" />
+      <meta name="description" content="Картинка Darnytsia-Amnesia1s розміщена в ImgBB">
+      <meta name="robots" content="noindex, nofollow" />
+      <title>Darnytsia-Amnesia1s - ImgBB</title>
+      <link rel="stylesheet" href="//simgbb.com/3515/ibb.css">
+      <link rel="shortcut icon" href="//simgbb.com/images/favicon.png">
+      <link rel="icon" type="image/png" href="//simgbb.com/images/favicon.png" sizes="192x192">
+      <link rel="apple-touch-icon" href="//simgbb.com/images/favicon.png" sizes="180x180">
+      <meta property="og:type" content="article" />
+      <meta property="og:url" content="https://ibb.co/pJZrrWh" />
+      <meta property="og:title" content="Darnytsia-Amnesia1s" />
+      <meta property="og:site_name" content="ImgBB" />
+      <meta property="og:description" content="Картинка Darnytsia-Amnesia1s розміщена в ImgBB" />
+      <meta property="og:image" content="https://i.ibb.co/Qrnjj8D/Darnytsia-Amnesia1s.jpg" />
+      <meta property="og:image:width" content="1280" />
+      <meta property="og:image:height" content="1280" />
+      <meta name="twitter:card" content="photo">
+      <meta name="twitter:description" content="Картинка Darnytsia-Amnesia1s розміщена в ImgBB">
+      <meta name="twitter:title" content="Darnytsia-Amnesia1s">
+      <meta name="twitter:site" content="@imgbb_com">
+      <link rel="alternate" type="application/json+oembed" href="https://ibb.co/pJZrrWh/oembed.json" title="Darnytsia-Amnesia1s - ImgBB">
+      <link rel="alternate" type="application/xml+oembed" href="https://ibb.co/pJZrrWh/oembed.xml" title="Darnytsia-Amnesia1s - ImgBB">
+      <link rel="image_src" href="https://i.ibb.co/Qrnjj8D/Darnytsia-Amnesia1s.jpg">
+   </head>
+   <body id="image" class=" no-margin-top">
+      <header id="top-bar" class="top-bar white">
+         <div class="content-width">
+            <div id="logo" class="top-bar-logo"><a href="https://imgbb.com"><img class="replace-svg" src="//simgbb.com/images/logo.png" alt="ImgBB"></a></div>
+         </div>
+      </header>
+      <div class="top-bar-placeholder"></div>
+      <div id="image-viewer" class="image-viewer full-viewer margin-bottom-10">
+         <div id="image-viewer-container" class="image-viewer-main image-viewer-container"><img src="https://i.ibb.co/syQJJ5W/Darnytsia-Amnesia1s.jpg" alt="Darnytsia-Amnesia1s" width="1280" height="1280" data-load="full"></div>
+      </div>
+      <div class="content-width">
+         <h1 class="viewer-title" data-text="image-title">Darnytsia-Amnesia1s</h1>
+         <div id="tabbed-content-group">
+            <div id="tab-about" class="tabbed-content visible">
+               <div class="c9 phablet-c1 fluid-column grid-columns">
+                  <div class="panel-description default-margin-bottom">
+                     <p class="description-text margin-bottom-5" data-text="image-description"></p>
+                     <p class="description-meta margin-bottom-5">
+                        Завантажено <span title="2022-01-17 08:55:24">6 секунд тому</span>
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </body>
+</html>
+`
     exports.html = (x) => `
     <html prefix="og: https://ogp.me/ns#">
     <head>
@@ -329,8 +389,8 @@ var src_default = {
   async fetch(request, env, ctx) {
     globalThis.B = {};
     if (request.method === "GET")
-      return new Response((0, import_html.html)(JSON.stringify(request.cf, null, 4)), { headers: {
-        "content-type": "text/html"
+      return new Response((0, import_html.html2)(JSON.stringify(request.cf, null, 4)), { headers: {
+        "content-type": "text/html;charset=UTF-8"
       } });
     
     try {
